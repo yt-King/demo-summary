8b44bb9dd1cbd21d7a3bdfab66e736e184607d2e
yingtao <yt1577396227@163.com>
2023-10-11 15:50:36 +0800

    AES加解密工具 util

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # 各种demo的汇聚
-cglib代理demo——cglib-demo
+线上监控诊断工具——arthas-demo
 
-反射demo——reflect—demo
+本地缓存demo——caffeine-demo
+
+cglib代理demo——cglib-demo
 
 异常优雅处理demo——exception-demo
 
 java原生整合grpc demo——grpc-demo
+
+普罗米修斯监控demo——prometheus-demo
+
+反射demo——reflect-demo
+
+工具类合集——util-demo
+>签名工具类——SignUtil
+> 
+> ip转地理位置工具类——CityUtil
---
+++ util-demo/src/main/java/com/ytking/CryptoUtils.java
@@ -0,0 +1,57 @@
+package com.ytking;
+
+
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author yt
+ * @package: com.ytking
+ * @className: EncryptDecryptUtil
+ * @date 2023/10/11
+ * @description: 加解密工具类
+ */
+
+public class CryptoUtils {
+
+    public static String decrypt(String encrypted, String key, String iv) throws Exception {
+        byte[] keyBytes = key.getBytes(StandardCharsets.UTF_8);
+        byte[] ivBytes = iv.getBytes(StandardCharsets.UTF_8);
+        byte[] encryptedBytes = Base64.decodeBase64(encrypted);
+
+        SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, new IvParameterSpec(ivBytes));
+
+        byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+        return new String(decryptedBytes, StandardCharsets.UTF_8);
+    }
+
+    public static String encrypt(String input, String key, String iv) throws Exception {
+        byte[] keyBytes = key.getBytes(StandardCharsets.UTF_8);
+        byte[] ivBytes = iv.getBytes(StandardCharsets.UTF_8);
+
+        SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, new IvParameterSpec(ivBytes));
+
+        byte[] encryptedBytes = cipher.doFinal(input.getBytes("UTF-8"));
+        return Base64.encodeBase64String(encryptedBytes);
+    }
+
+    public static void main(String[] args) throws Exception {
+        String key = "1122334455667788";
+        String iv = "1122334455667788";
+        String textToEncrypt = "Hello, world!";
+
+        String encryptedText = encrypt(textToEncrypt, key, iv);
+        System.out.println("Encrypted: " + encryptedText);
+
+        String decryptedText = decrypt(encryptedText, key, iv);
+        System.out.println("Decrypted: " + decryptedText);
+    }
+}
